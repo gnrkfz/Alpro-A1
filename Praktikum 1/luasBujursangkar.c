e08696566f7b9c89bd27e9eabaa43a9bd71d9f59
Ganar KF <gnrkfz@gmail.com>
2022-03-01 11:10:36 +0700

    Add files via upload

---
+++ Praktikum 1/luasBujursangkar.c
@@ -0,0 +1,18 @@
+/* Nama File	: luasBujursangkar.c */
+/* Deskripsi	: menghitung luas bujursangkar */
+/* Pembuat	: Ganar Khatir Faza Zain - 24060121130079 */
+/* Tanggal	: Selasa, 1 Maret 2022 */
+
+#include <stdio.h>
+
+int main() {
+   int Sisi;
+   int Luas;
+
+   printf("Masukan Sisi: ");
+   scanf("%d",&Sisi);
+
+   printf("%d", sisi*sisi);
+
+   return 0;
+}
