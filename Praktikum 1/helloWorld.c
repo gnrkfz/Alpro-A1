e2f36e5da8037bcd5e56beb2a3f913c91578eda2
Ganar KF <90145379+gnrkfz@users.noreply.github.com>
2022-03-08 12:45:18 +0700

    Update helloWorld.c

--- Praktikum 1/helloWorld.c
+++ Praktikum 1/helloWorld.c
@@ -1,3 +1,8 @@
+/* Nama File	: helloWorld.c */
+/* Deskripsi	: menampilkan teks */
+/* Pembuat	: Ganar Khatir Faza Zain - 24060121130079 */
+/* Tanggal	: Selasa, 22 Maret 2022 */
+
 #include <stdio.h>
 
 int main() {
