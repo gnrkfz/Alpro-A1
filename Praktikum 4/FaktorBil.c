0755ebd576075efa1e71739c2dfb4bdcabb24972
Ganar KF <90145379+gnrkfz@users.noreply.github.com>
2022-03-15 13:16:00 +0700

    Update FaktorBil.c

--- Praktikum 4/FaktorBil.c
+++ Praktikum 4/FaktorBil.c
@@ -1,8 +1,8 @@
-/* Nama File	  : FaktorBil.c */
-/* Deskripsi	  : untuk menentukan faktor-faktor bilangan dari bilangan integer sembarang
-                  N (N > 0) */
-/* Pembuat	    : Ganar Khatir Faza Zain - 24060121130079 */
-/* Tanggal	    : Selasa, 15 Maret 2022 */
+/* Nama File	: FaktorBil.c */
+/* Deskripsi	: untuk menentukan faktor-faktor bilangan dari bilangan integer sembarang
+                N (N > 0) */
+/* Pembuat	  : Ganar Khatir Faza Zain - 24060121130079 */
+/* Tanggal	  : Selasa, 15 Maret 2022 */
 
 #include <stdio.h>
 #include <stdlib.h>
