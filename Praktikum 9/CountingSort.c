81690c18c2936bb3b33553fd6888c7d14eaaccfb
Ganar KF <90145379+gnrkfz@users.noreply.github.com>
2022-04-26 12:33:11 +0700

    Update CountingSort.c

--- Praktikum 9/CountingSort.c
+++ Praktikum 9/CountingSort.c
@@ -1,8 +1,8 @@
 /* Nama File	: CountingSort.c */
 /* Deskripsi	: Algoritma sorting dengan cara menghitung jumlah kemunculan dari setiap
-                data yang berada di dalam array */
-/* Pembuat	  : Ganar Khatir Faza Zain - 24060121130079 */
-/* Tanggal	  : Selasa, 19 April 2022 */
+                  data yang berada di dalam array */
+/* Pembuat	: Ganar Khatir Faza Zain - 24060121130079 */
+/* Tanggal	: Selasa, 19 April 2022 */
 
 #include <stdio.h>
 #include <stdlib.h>
