c33d698aacd7023e1f3c9e83c5debd82593d7a37
Ganar KF <90145379+gnrkfz@users.noreply.github.com>
2022-04-26 12:32:05 +0700

    Update and rename BubbleSort.c to Praktikum 9/BubbleSort.c

--- Praktikum 9/BubbleSort.c
+++ Praktikum 9/BubbleSort.c
@@ -1,13 +1,13 @@
 /* Nama File	: BubbleSort.c */
 /* Deskripsi	: Algoritma sorting dengan melakukan penukaran data secara terus menerus
-                  sampai urut */
-/* Pembuat	    : Ganar Khatir Faza Zain - 24060121130079 */
-/* Tanggal	    : Selasa, 19 April 2022 */
+                sampai urut */
+/* Pembuat	  : Ganar Khatir Faza Zain - 24060121130079 */
+/* Tanggal	  : Selasa, 19 April 2022 */
 
 #include <stdio.h>
+#include <stdlib.h>
 
-void bubbleSort(int arr[], int n)
-{
+void bubbleSort(int arr[], int n){
     int i, j, temp;
     for (i = 0; i < n - 1; i++){
         for (j = 0; j < n - i - 1; j++){
@@ -20,8 +20,7 @@ void bubbleSort(int arr[], int n)
     }
 }
 
-int main()
-{
+int main(){
     int i, n;
     n = 10;
     int arr[] = {6,10,3,9,7,6,99,88,91,25};
